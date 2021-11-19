9761a0266140e309cfd40de0a5b2e18b249d0ea4
Savanbeladiya <84185026+Savanbeladiya@users.noreply.github.com>
2021-11-19 18:14:16 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -13,14 +13,14 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-         .package(url: "http://github.com/Savanbeladiya/GithubActionDemo", from: "1.0.0"),
+         .package(url: "http://github.com/Savanbeladiya/GithubActionDemo", from: "1.0.1"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "GithubActionDemo",
-            dependencies: [GithubActionDemo]),
+            dependencies: ["GithubActionDemo"]),
         .testTarget(
             name: "GithubActionDemoTests",
             dependencies: ["GithubActionDemo"]),
