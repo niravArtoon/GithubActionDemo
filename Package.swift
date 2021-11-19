2e438375026a53a6beb0787275fe92fe23da7c7b
Savanbeladiya <84185026+Savanbeladiya@users.noreply.github.com>
2021-11-19 13:53:22 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -12,6 +12,7 @@ let package = Package(
             targets: ["GithubActionDemo"]),
     ],
     dependencies: [
+        .package(url: "https://github.com/Savanbeladiya/GithubActionDemo.git")
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
     ],
