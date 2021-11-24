224c872b3674ce86c0f5c309bbd9226f855c0d4b
Savanbeladiya <84185026+Savanbeladiya@users.noreply.github.com>
2021-11-24 12:16:10 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "GithubActionDemo",
-            dependencies: [""]),
+            dependencies: []),
         .testTarget(
             name: "GithubActionDemoTests",
             dependencies: ["GithubActionDemo"]),
