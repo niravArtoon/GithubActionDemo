3da52481dae834c5487f0ca87e4c53136916eb2b
Savanbeladiya <84185026+Savanbeladiya@users.noreply.github.com>
2021-11-24 18:07:00 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,13 +4,13 @@
 import PackageDescription
 
 let package = Package(
-    name: "GithubActionDemo1",
+    name: "GithubActionDemo",
     
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "GithubActionDemo1",
-            targets: ["GithubActionDemo1"]),
+            name: "GithubActionDemo",
+            targets: ["GithubActionDemo"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -21,10 +21,10 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "GithubActionDemo1",
-            dependencies: ["GithubActionDemo1"]),
+            name: "GithubActionDemo",
+            dependencies: ["GithubActionDemo"]),
         .testTarget(
             name: "GithubActionDemoTests",
-            dependencies: ["GithubActionDemo1"]),
+            dependencies: ["GithubActionDemo"]),
     ]
 )
